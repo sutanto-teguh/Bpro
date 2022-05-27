c491a9bbd3cfaa2bf63480aa273ebf2e3cf3f0a9
sutanto-teguh <teguh@dinamika.ac.id>
2022-05-27 14:48:58 +0700

    Pert 8-2

---
+++ pert8/ContohMethod00.java
@@ -0,0 +1,17 @@
+public class ContohMethod00 {
+    public static void main(String[] args) {
+        for(int i=0;i<20;i++){
+            System.out.print("*");
+        }
+        System.out.println("");
+        System.out.println("Program Penjualan");
+        for(int i=0;i<20;i++){
+            System.out.print("*");
+        }
+        System.out.println("");
+        for(int i=0;i<20;i++){
+            System.out.print("*");
+        }
+        System.out.println("");
+    }
+}
---
+++ pert8/ContohMethod02.java
@@ -0,0 +1,6 @@
+public class ContohMethod02 {
+    public static void main(String[] args) {
+        ContohMethod01.cetakGaris();
+        ContohMethod01.cetakGaris(30);
+    }
+}
